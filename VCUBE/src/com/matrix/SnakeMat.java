440b956bf5585406f230f1541ceda2c6eaee24e6
Ranith-45 <uppuranjith04@gmail.com>
2026-08-25 00:19:55 +0530

    day 16 commit

---
+++ VCUBE/src/com/matrix/SnakeMat.java
@@ -0,0 +1,48 @@
+package com.matrix;
+import java.util.*;
+public class SnakeMat {
+
+	
+
+	
+
+	    static ArrayList<Integer> snakePattern(int matrix[][]) {
+
+	        ArrayList<Integer> list = new ArrayList<>();
+
+	        for (int i = 0; i < matrix.length; i++) {
+
+	            if (i % 2 == 0) {
+
+	                // Left to right
+	                for (int j = 0; j < matrix[i].length; j++) {
+	                    list.add(matrix[i][j]);
+	                }
+
+	            } else {
+
+	                // Right to left
+	                for (int j = matrix[i].length - 1; j >= 0; j--) {
+	                    list.add(matrix[i][j]);
+	                }
+	            }
+	        }
+
+	        return list;
+	    }
+
+	    public static void main(String[] args) {
+
+	        int matrix[][] = {
+	            {1, 2, 3},
+	            {4, 5, 6},
+	            {7, 8, 9}
+	        };
+
+	        ArrayList<Integer> result = snakePattern(matrix);
+
+	        System.out.println(result);
+	    }
+	}
+
+
