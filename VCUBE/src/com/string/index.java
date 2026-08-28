dc568107cc2cd2238b2e3811835d174061baa4aa
Ranith-45 <uppuranjith04@gmail.com>
2026-08-28 22:17:52 +0530

    day 17 commit

--- VCUBE/src/com/string/index.java
+++ VCUBE/src/com/string/index.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class index {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+       
         System.out.println("Enter the string:");
         String s = sc.nextLine();
 
